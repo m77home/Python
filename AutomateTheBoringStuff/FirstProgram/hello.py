e761756dcde982726a8dce024886735e5834337a
m77 <m77.home@gmail.com>
2017-11-17 18:18:00 +0100

    First version of hello.py

---
+++ AutomateTheBoringStuff/FirstProgram/hello.py
@@ -0,0 +1,8 @@
+# This program says Hello and asks for your name
+
+print('Hello world!')
+print('What is your name?')
+myName=input()
+print('It is good to meet you ' + myName)
+print('The length of your name is:')
+print(len(myName))
