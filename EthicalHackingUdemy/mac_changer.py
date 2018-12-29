9fdcaedbea3cdff606dd595583de48fb94206cf3
m77home <m77.home@gmail.com>
2018-12-29 18:42:23 -0500

    Finished!

--- EthicalHackingUdemy/mac_changer.py
+++ EthicalHackingUdemy/mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 
 def get_arguments():
@@ -15,6 +16,7 @@ def get_arguments():
         parser.error("[-] Please specify a new MAC, use --help for info")
     return options
 
+
 def change_mac(interface, new_mac):
     print("[+] Changing mac for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
@@ -22,5 +24,22 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read the MAC address...")
+
+
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
 change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to " + current_mac)
+else:
+    print("[-] Something went wrong, MAC wasn't changed...")
