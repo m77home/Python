25eb16e8328057c59b7a5c1511cd2679986eed73
m77 <m77.home@gmail.com>
2019-01-06 23:01:40 +0100

    First

---
+++ PythonMasterClassUdemy/HelloWorld.py
@@ -0,0 +1,28 @@
+sentence = "Scandinavian countries are the best"
+
+print(sentence)
+
+# print character at index in string
+print(sentence[0])
+print(sentence[1])
+print(sentence[-1])
+print()
+
+# slices of strings
+print(sentence[0:5])
+print(sentence[:5])
+print(sentence[3:])
+print(sentence[0:6:2])
+print()
+
+# string operators
+string1 = "First "
+string2 = "Second "
+print(string1 + string2)
+print(1*string1 + 2*string2)
+print()
+
+today = "sunday"
+print("day" in today)
+print("sun" in today)
+print("hello" in today)
